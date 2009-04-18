4a3a56992eba25508262445c07724db4b5285459
Marc Bowes <marcbowes@gmail.com>
2009-04-18 16:59:16 +0200

    merge fix

--- src/mxit/client.h
+++ src/mxit/client.h
@@ -21,43 +21,6 @@ class Client : public QObject
   public:         /* class specific */
 	
   Client(QObject *parent = 0);
-
-<<<<<<< HEAD:src/mxit/client.h
-	
-
-  public:         /* methods */
-    
-  private:        /* methods */
-
-  private:        /* variables */
-
-=======
-	signals:
-	
-  void captchaReceived(const QByteArray &);
-	
-	private slots:
-	
-  void httpRequestStarted(int requestId);
-  void httpRequestFinished(int requestId, bool error);
-
-  public:         /* methods */
-  
-  void getLoginCaptcha();
-  void sendChallengeResponse(QString captchaResponse, QString login);
-    
-  private:        /* methods */
-  
-  QByteArray extractDataFromResponce(int data_num);
-
-	private:        /* variables */
-	
-  QMutex         *captchaMutex;
-  QWaitCondition *captchaWaitCond;
-  QHttp          *http;
-  int             httpGetId;
-  QByteArray      responseByteArray;
->>>>>>> 68e02e608c65acfd1b1424b7277151a6894e7d63:src/mxit/client.h
 };
 
 }
