4818291f89b5fe43db147c4c67a90c90f3196e79
Tim Sjoberg <bedlamp@gmail.com>
2009-04-18 12:38:17 +0000

    trying to get QHttp to work properly

--- src/mxit.cpp
+++ src/mxit.cpp
@@ -1,18 +1,37 @@
 #include "mxit.h"
 #include <QString>
 #include <QUrl>
-#include <QHttp>
-#include <QFile>
 #include <QDateTime>
-#include <iostream>
-#include <string>
+#include <QDebug>
 
 Mxit::Mxit(QObject *parent) : QObject (parent)
 {
   QString timestamp = QString("%1").arg(QDateTime::currentDateTime().toTime_t());
-  QUrl url("http://www.mxit.com/res/?type=challenge&getcountries=true&getlanguage=true&getimage=true&ts=" + timestamp);
-  //QHttp http(this);
-  //QFile file("testfile");
+  //QUrl url("http://www.mxit.com/res/");//?type=challenge&getcountries=true&getlanguage=true&getimage=true&ts=" + timestamp);
+  QUrl url("http://war3.co.za/forum/viewforum.php");
+  http = new QHttp(this);
+    
+  connect(http, SIGNAL(requestFinished(int, bool)), this, SLOT(httpRequestFinished(int, bool)));
   
-  std::cout << url.toString().toStdString() << std::endl;
+  QByteArray query;
+  query += url.path().toLatin1();
+  //query += "?type=challenge&getcountries=true&getlanguage=true&getimage=true&ts=";
+  //query += timestamp;
+  query += "?f=26&sid=194feef8a58e637d463271796dd23d22";
+  
+  qDebug() << query << "\n";
+  
+  http->setHost(url.host(), url.port() == -1 ? 0 : url.port());
+  httpGetId = http->get(query);
+}
+
+void Mxit::httpRequestFinished(int requestId, bool error)
+{
+  QByteArray response = http->readAll();
+  QString temp(response);
+  if (!error)
+    qDebug() << "success\n";
+  else
+    qDebug() << "err0r\n";
+  qDebug() << temp << "\n";
 }
