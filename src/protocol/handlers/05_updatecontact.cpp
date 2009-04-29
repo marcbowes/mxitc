7780384157d0d9608858fbaf265a78666b29c331
Tim Sjoberg <bedlamp@gmail.com>
2009-04-29 18:53:25 +0000

    added skeleton for updatecontact packet

---
+++ src/protocol/handlers/05_updatecontact.cpp
@@ -0,0 +1,46 @@
+/****************************************************************************
+**
+** For Copyright & Licensing information, see COPYRIGHT in project root
+**
+****************************************************************************/
+
+#include "05_updatecontact.h"
+
+namespace MXit
+{
+
+namespace Protocol
+{
+
+namespace Handlers
+{
+
+/****************************************************************************
+**
+** Author: Tim Sjoberg
+**
+** Populates a packet with the information required to logout
+**
+****************************************************************************/
+void UpdateContact::build(MXit::Network::Packet *packet, VariableHash &variables)
+{
+  
+}
+
+/****************************************************************************
+**
+** Author: Tim Sjoberg
+**
+** Extracts variable information from the logout packet
+**
+****************************************************************************/
+VariableHash UpdateContact::handle(const QByteArray &packet)
+{
+  
+}
+
+}
+
+}
+
+}
---
+++ src/protocol/handlers/05_updatecontact.h
@@ -0,0 +1,48 @@
+/****************************************************************************
+**
+** For Copyright & Licensing information, see COPYRIGHT in project root
+**
+** Author: Tim Sjoberg, 2009
+**
+** MXit::Network::Handlers::UpdateContact deals with updating a specific contacts details
+** see build/handle definitions in .cpp file for implementation details
+**
+****************************************************************************/
+
+#ifndef __MXIT_PROTOCOL_HANDLERS_LOGOUT_H__
+#define __MXIT_PROTOCOL_HANDLERS_LOGOUT_H__
+
+#include <QObject>
+
+#include "protocol/handler.h"
+
+namespace MXit
+{
+
+namespace Protocol
+{
+
+namespace Handlers
+{
+
+class UpdateContact : public Handler
+{
+  Q_OBJECT
+  
+  public:         /* methods */
+  
+  virtual void build(MXit::Network::Packet *packet, VariableHash &variables);
+  virtual VariableHash handle(const QByteArray &packet);
+  
+  public:         /* variables */
+  
+  static const unsigned int command = 5;
+};
+
+}
+
+}
+
+}
+
+#endif
