a6ba78a5d732a15e159687ff28fb85e013612254
Tim Sjoberg <bedlamp@gmail.com>
2009-04-30 17:15:43 +0000

    finished getmessages receive

--- src/protocol/handlers/09_getmessages.cpp
+++ src/protocol/handlers/09_getmessages.cpp
@@ -89,9 +89,10 @@ VariableHash GetMessages::handle(const QByteArray &packet)
   **  
   **  Flags:
   **    The following flags are defined:
-  **      0x2             Request delivery notification (only available to services)
-  **      0x4             Request read notification, i.e. only when message if displayed on screen
-  **                      notification should be sent.
+  **      0x2             Request delivery notification (only available to 
+  **                      services)
+  **      0x4             Request read notification, i.e. only when message 
+  **                      if displayed on screen notification should be sent.
   **      0x10            Password encrypted
   **      0x20            Transport encrypted
   **      0x40            Reply should be sent password encrypted
@@ -103,7 +104,29 @@ VariableHash GetMessages::handle(const QByteArray &packet)
 
   */
   
+  /* setup */
+  StringVec variables;
   
+  /* first break up packet by \0 into variable sections */
+  variables.append("ln");                   /* ln=X\0 */
+  variables.append("command");              /* 8\0 */
+  variables.append("error");                /* errorCode[\1errorMessage]\0 */
+  variables.append("contactData");          /* contactAddress \1 dateTime \1 type [ \1 id \1 flags ]\0 */
+  variables.append("message");              /* msg */
+  
+  /* extract \0 seperated values */
+  VariableHash rawMessage = hashVariables(packet, variables, "\0");
+  
+  variables.clear();
+  variables.append("contactAddress");
+  variables.append("dateTime");
+  variables.append("type");
+  variables.append("id");
+  variables.append("flags");
+  
+  VariableHash contactDetails = hashVariables(rawMessage["contactData"], variables, "\1");
+  
+  return rawMessage.unite(contactDetails);
 }
 
 }
