d03eb23c2ab499a54c5edf6cff98db35d118a21e
Marc Bowes <marcbowes@gmail.com>
2009-05-08 23:03:55 +0200

    added skeleton for 27_getmultimediamessage

---
+++ src/protocol/handlers/27_getmultimediamessage.cpp
@@ -0,0 +1,77 @@
+/****************************************************************************
+**
+** For Copyright & Licensing information, see COPYRIGHT in project root
+**
+****************************************************************************/
+
+#include "27_getmultimediamessage.h"
+
+namespace MXit
+{
+
+namespace Protocol
+{
+
+namespace Handlers
+{
+
+/****************************************************************************
+**
+** Author: Tim Sjoberg
+**
+** Populates a packet with the information required to request binary data
+**
+****************************************************************************/
+void GetMultimediaMessage::build(MXit::Network::Packet *packet, VariableHash &variables)
+{
+  /*
+  == PACKET FORMAT
+  ***************************************************************************
+  **
+  **  id=loginname[\1sesid]\0
+  **  cm=27\0
+  **  ms=chunkedData
+  **
+  ***************************************************************************
+  
+  == DEFINITIONS
+  ***************************************************************************
+  **
+  **  chunkedData         see appendix G. too long to pasted
+  **
+  ***************************************************************************
+  */
+  
+  /* packet header setup */
+  packet->setCommand("27");
+  
+  /* packet data setup */
+  (*packet) << "oogelyboogely";//FIXME: not a real request :)
+}
+
+/****************************************************************************
+**
+** Author: Tim Sjoberg
+**
+** Extracts nothing at the moment
+**
+****************************************************************************/
+VariableHash GetMultimediaMessage::handle(const QByteArray &packet)
+{
+  /*
+  == PACKET FORMAT
+  ***************************************************************************
+  **
+  **  27\0
+  **  errorCode[\1errorMessage]\0
+  **  chunkedData
+  **
+  ***************************************************************************
+  */
+}
+
+}
+
+}
+
+}
