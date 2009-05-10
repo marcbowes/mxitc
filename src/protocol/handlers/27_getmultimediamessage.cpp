773a5fac73ce954eb6196766d70bdf0c0a2ae0fa
Marc Bowes <marcbowes@gmail.com>
2009-05-10 21:44:41 +0200

    compiler semantics

--- src/protocol/handlers/27_getmultimediamessage.cpp
+++ src/protocol/handlers/27_getmultimediamessage.cpp
@@ -80,7 +80,7 @@ VariableHash GetMultimediaMessage::handle(const QByteArray &packet)
   
   int i=0, count = 0;
   
-  while (count < packet.startsWith("ln=") ? 3 : 2) {
+  while (count < (packet.startsWith("ln=") ? 3 : 2)) {
     i++;
     if (packet.at(i) == '\0')
       count++;
