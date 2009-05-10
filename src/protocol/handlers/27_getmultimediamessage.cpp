c0f742abdd9dd56d0cb40fa1500fae40c6ba567b
Marc Bowes <marcbowes@gmail.com>
2009-05-10 19:06:55 +0200

    added 27 handler to client.cpp started handling packet type 1 in 27_getmultimediamessage.cpp

--- src/protocol/handlers/27_getmultimediamessage.cpp
+++ src/protocol/handlers/27_getmultimediamessage.cpp
@@ -5,6 +5,7 @@
 ****************************************************************************/
 
 #include "27_getmultimediamessage.h"
+#include <QTextCodec>
 
 namespace MXit
 {
@@ -75,18 +76,115 @@ VariableHash GetMultimediaMessage::handle(const QByteArray &packet)
   **
   ***************************************************************************
   */
-  /* setup */
-  StringVec variables;
   
-  /* first break up packet by \0 into variable sections */
-  variables.append("ln");                   /* ln=X\0 */
-  variables.append("command");              /* 27\0 */
-  variables.append("error");                /* errorCode[\1errorMessage]\0 */
-  variables.append("chunkedData");
+  int i=0, count = 0;
   
-  VariableHash data = hashVariables(packet, variables, '\0');
+  while (count < 3) {
+    i++;
+    if (packet.at(i) == '\0')
+      count++;
+  }
+  
+  QByteArray chunkedData = packet.right(packet.size() - i - 1);
+  
+  int position = 0;
+  int size = chunkedData.size();
+  
+  //while (position < chunkedData.size()) {
+    qDebug() << "position at type: " << position;
+    //getting type byte
+    int type = (int)chunkedData.at(position);
+    qDebug() << "type: " << type;
     
-  return data;
+    position++;
+    
+    qDebug() << "position at length: " << position;
+    //now at first length byte. getting length
+    //watch this haxx
+    int length = 0;
+    length |= chunkedData[position];
+    qDebug() << "length: " << length;
+    position++;
+    for (int j=0; j<3; j++) {
+      qDebug() << "length before shift: " << length;
+      length = length << 8;
+      qDebug() << "length after shift: " << length;
+      length |= (unsigned char)chunkedData[position + j];
+    }
+    position += 3;
+    //random null terminator. nfc what its for
+    position++;
+    
+    qDebug() << "size: " << size;
+    qDebug() << "length: " << length;
+    qDebug() << "position + length: " << position + length;
+    
+    qDebug() << "position at data: " << position;
+    //if (length < 0) break;
+    
+    //now at first data byte
+    //getting data
+    QByteArray data = chunkedData.mid(position, length);
+    
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+    
+    int penis = 0;
+    int tdpenis = 0;
+    QByteArray tempData;
+    while (data.at(penis) != '\0') {
+      tempData[tdpenis] = data[penis];
+      penis++;
+      tdpenis++;
+    }
+    qDebug() << "id: " << codec->toUnicode(tempData);
+    
+    penis++;
+    tempData.clear();
+    tdpenis = 0;
+    
+    while ((data.at(penis) != '\0')&&(data.at(penis) != '\1')) {
+      tempData[tdpenis] = data[penis];
+      penis++;
+      tdpenis++;
+    }
+    
+    qDebug() << "handle: " << codec->toUnicode(tempData);
+    
+    //no null to dodge
+    qDebug() << "operation: " << (int)data.at(penis);
+    
+    penis++; // moving on
+    
+    int chunkLength = 0;
+    chunkLength |= data[penis];
+    qDebug() << "chunk length debug";
+    qDebug() << chunkLength;
+    penis++;
+    for (int j=0; j<3; j++) {
+      chunkLength = chunkLength << 8;
+      qDebug() << "before: " << chunkLength;
+      chunkLength |= (unsigned char)data[penis + j];
+      qDebug() << "what it is: " << (int)(unsigned char)data[penis + j];
+      qDebug() << "after: " << chunkLength;
+    }
+    penis += 3;
+    
+    qDebug() << "chunk length: " << chunkLength;
+    
+    penis += chunkLength;
+    
+    qDebug() << "penis: " << penis;
+    qDebug() << "total: " << data.size();
+    
+    //moving to next chunk
+    position += length;
+    
+    
+    
+    //qDebug() << string;
+  //}
+  
+  return VariableHash();
 }
 
 }
