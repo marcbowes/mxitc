49e9a872bc455cb1707bb9da2f5bc5beb877044e
Marc Bowes <marcbowes@gmail.com>
2009-05-13 22:22:24 +0200

    stub of MXit::Message, which will become part of the refactored chat session management

---
+++ src/mxit/message.h
@@ -0,0 +1,20 @@
+#ifndef __MXIT_MESSAGE_H__
+#define __MXIT_MESSAGE_H__
+
+namespace MXit
+{
+
+class Message
+{
+  public:         /* class specific */
+  
+  Message();
+  ~Message();
+
+  public:         /* methods */
+};
+
+}
+
+#endif /* __MXIT_MESSAGE_H__ */
+
