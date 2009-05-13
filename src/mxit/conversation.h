08118131012f678a723a85448008f4ccf18b4125
Marc Bowes <marcbowes@gmail.com>
2009-05-13 21:28:44 +0200

    stub of MXit::Conversation, which will become a v2 of chat session management

---
+++ src/mxit/conversation.h
@@ -0,0 +1,30 @@
+/****************************************************************************
+**
+** For Copyright & Licensing information, see COPYRIGHT in project root
+**
+** Author: Marc Bowes, 2009
+**
+** MXit::Conversation is essentially a join model between contacts, where
+** the goal is to communicate. There can be an arbitrary number of contacts
+** in this model.
+**
+****************************************************************************/
+
+#ifndef __MXIT_CONVERSATION_H__
+#define __MXIT_CONVERSATION_H__
+
+namespace MXit
+{
+
+class Conversation
+{
+  public:         /* class specific */
+	
+  Conversation();
+  ~Conversation();
+};
+
+}
+
+#endif /* __MXIT_CONVERSATION_H__ */
+
