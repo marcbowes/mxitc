0a669d3fdef36b37c0927dc545be7df861771940
Marc Bowes <marcbowes@gmail.com>
2009-05-16 16:01:46 +0200

    implemented Client#updateProfile

--- src/mxit/client.cpp
+++ src/mxit/client.cpp
@@ -362,9 +362,17 @@ void Client::updateContactInfo()
 ** Author: Marc Bowes
 **
 ****************************************************************************/
-void Client::updateProfile()
+void Client::updateProfile(const QString &pin, const QString &name, bool hiddenLoginname,
+    const QDate &dateOfBirth, const QString &gender)
 {
-  /* FIXME: stub */
+  VariableHash profileVariables;
+  profileVariables["pin"]             = pin.toUtf8();
+  profileVariables["name"]            = name.toUtf8();
+  profileVariables["hiddenLoginname"] = hiddenLoginname ? "1" : "0";
+  profileVariables["dateOfBirth"]     = dateOfBirth.toString(Qt::ISODate).toUtf8();
+  profileVariables["gender"]          = gender.toLower() == "male" ? "1" : "0";
+  
+  sendPacket("updateprofile", profileVariables);
 }
 
 
