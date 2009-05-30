0f0b4877def977200aaa15274f7543cf1e3f82c0
Marc Bowes <marcbowes@gmail.com>
2009-05-30 22:47:42 +0200

    created emoticon base class

---
+++ src/gui/emoticon.cpp
@@ -0,0 +1,20 @@
+/****************************************************************************
+**
+** For Copyright & Licensing information, see COPYRIGHT in project root
+**
+****************************************************************************/
+
+#include "emoticon.h"
+
+namespace MXit
+{
+
+namespace GUI
+{
+
+/* STUB */
+
+}
+
+}
+
---
+++ src/gui/emoticon.h
@@ -0,0 +1,34 @@
+/****************************************************************************
+**
+** For Copyright & Licensing information, see COPYRIGHT in project root
+**
+** Author: Marc Bowes, 2009
+**
+** Class for emoticon converting between English, shorthand and HTML.
+**
+****************************************************************************/
+
+#ifndef __MXIT_GUI_EMOTICON_H__
+#define __MXIT_GUI_EMOTICON_H__
+
+
+namespace MXit
+{
+
+namespace GUI
+{
+
+class Emoticon
+{
+  public:         /* class specific */
+
+  Emoticon()  {}
+  ~Emoticon() {}
+};
+
+}
+
+}
+
+#endif /* __MXIT_GUI_EMOTICON_H__ */
+
